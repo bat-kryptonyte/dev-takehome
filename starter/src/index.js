403b6ce92eb3b6058131adfcc229e2d9b8c5fe1c
enzowagaokada <enzookada37@gmail.com>
2025-05-27 12:17:11 -0400

    Completed tasks

--- starter/src/index.js
+++ starter/src/index.js
@@ -28,13 +28,149 @@ res.json({ healthy: true });
 
 // — Level 1 CRUD endpoints —
 // POST /api/user       → create a new User
+const ObjectID = mongoose.Schema.Types.ObjectId;
+const User = new mongoose.model('User', new mongoose.Schema({
+    _id: {
+        type: ObjectID,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: false
+    },
+    createdAt: {
+        type: Date,
+        required: true
+    }
+}));
+
+app.post('/api/users', async (req, res) => {
+    try {
+        const newUser = new User(req.body);
+        newUser.save();
+        res.status(200).json({message : "User saved sucessfully."});
+    } catch (err) {
+        res.status(400).json({error : err.message});
+    }
+}
+);
+
 // POST /api/book       → create a new Book
+
+const Book = new mongoose.model('Book', new mongoose.Schema({
+    _id: {
+        type: ObjectID,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    ownerID: {
+        type: ObjectID,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+    }
+}));
+
+app.post('/api/book', async (req, res) => {
+    try {
+        const newBook = new Book(req.body);
+        newBook.save();
+        res.status(200).json({message : "Book saved sucessfully."});
+    } catch (err) {
+        res.status(400).json({error : err.message});
+    }
+});
+
 // POST /api/reading    → create a new ReadingLog
 
+const ReadingLog = new mongoose.model('ReadingLog', new mongoose.Schema({
+    _id: {
+        type: ObjectID,
+        required: true,
+    },
+    bookId: {
+        type: ObjectID,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    notes: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        required: true
+    }
+}));
+
+app.post('/api/reading', async (req, res) => {
+    try {
+        const userBookId = req.body.bookId;
+        const userBook = Book.findOne(userBookId);
+        if (!userBook.ownerId.equals(userBookId)) {
+            res.status(400).json({message : "bookId does not match with ownerId."});
+        }
+        const newLog = new ReadingLog(req.body);
+        newLog.save();
+        res.status(200).json({message : "Reading Log saved sucessfully."});
+    } catch (err) {
+        res.status(400).json({error : err.message});
+    }
+});
+
 // — Level 2 Admin views —
+
 // GET  /api/admin/users    → list all Users
+
+app.get('/api/admin/users', async (req, res) => {
+    try {
+        const users = User.find();
+        res.status(200).json(users);
+    } catch(err) {
+        res.status(400).json({error : err.message});
+    }
+});
+
 // GET  /api/admin/books    → list all Books
+app.get('/api/admin/books', async (req, res) => {
+    try {
+        const books = Book.find();
+        res.status(200).json(books);
+    } catch(err) {
+        res.status(400).json({error : err.message});
+    }
+});
+
 // GET  /api/admin/reading  → list all ReadingLogs
+app.get('/api/admin/reading', async (req, res) => {
+    try {
+        const logs = ReadingLog.find();
+        res.status(200).json(logs);
+    } catch(err) {
+        res.status(400).json({error : err.message});
+    }
+});
 
 // — Level 3 Authentication (optional) —
 // POST /api/user/login     → authenticate and issue JWT
